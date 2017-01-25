b5f71ae6faa06414a0c251cabeee4ee6f44ef7cc
lucasmuileboom <muileboomlucas@gmail.com>
2017-01-25 14:43:54 +0100

    coin

---
+++ Assets/Scripts/LoseCoins.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCoins : MonoBehaviour
+{
+    //GameObject Player = GameObject.Find("Player");
+    //coinpickup coinpickup = Player.GetComponent<coinpickup>();
+    [SerializeField]
+    GameObject coin;
+    [SerializeField]
+    List<GameObject> Spawning = new List<GameObject>();
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            //spawn coins
+            for (int i = 0; i < Spawning.Count; i++) {
+                Instantiate(coin, Spawning[i].transform.position,Quaternion.identity);
+            }
+
+            /*Coins = Coins - 6;
+            if (Coins < 0)
+            {
+                Coins = 0;
+            }*/
+        }
+    }
+}
---
+++ Assets/Scripts/coinrotation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coinrotation : MonoBehaviour
+{
+    public int Speed;
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Speed * Time.deltaTime);
+    }
+}
