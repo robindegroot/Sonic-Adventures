c6a359c2ecf99b7bc1c9893e28de92107237ed6d
robindegroot <robindegroot.rdg@gmail.com>
2017-01-27 13:35:09 +0100

    update3

---
+++ Assets/TargetPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime;
+using UnityEngine;
+
+public class TargetPlayer : MonoBehaviour
+{
+    public Transform target;
+
+
+    private void Update ()
+    {
+        transform.LookAt(target);
+    }
+}
