b20ad1c828a04127a4f3465461b56deb759347d9
robindegroot <robindegroot.rdg@gmail.com>
2017-01-27 22:25:02 +0100

    scene switcher

---
+++ Assets/Scripts/EscapeScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeScript : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            if (Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+                Debug.Log("Pauzed");
+            }
+            else {
+                Time.timeScale = 0;
+            }
+        }
+
+
+    }
+    
+}
